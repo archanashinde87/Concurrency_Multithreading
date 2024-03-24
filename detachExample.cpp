f5278f6363e42c8f8ae301bc9716466af9bc2b71
Archana Shinde <144507091+archanashinde87@users.noreply.github.com>
2024-03-24 08:56:35 +0530

    Add files via upload
    example to show how detach() works

---
+++ detachExample.cpp
@@ -0,0 +1,31 @@
+#include <chrono>
+#include <iostream>
+#include <thread>
+ 
+void independentThread() 
+{
+    std::cout << "Starting concurrent thread.\n";
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::cout << "Exiting concurrent thread.\n";
+}
+ 
+void threadCaller() 
+{
+    std::cout << "Starting thread caller.\n";
+    std::thread t(independentThread);
+    t.detach();
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::cout << "Exiting thread caller.\n";
+}
+ 
+int main() 
+{
+    threadCaller();
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+}
+
+// Result - as independentThread detached main doesn't wait to finish it...if detached thread 
+// doesn't finish till main finishes it will get suspended.
+//Starting thread caller.
+//Starting concurrent thread.
+//Exiting thread caller.
